lioniac/pokefirered_dev

[Fix] Multiple Quick Bugfixes

src/evolution_scene.c
@@ -1089,9 +1089,9 @@ static void Task_TradeEvolutionScene(u8 taskId)
          * BUG: This check causes the evolved Pokemon's cry to play over the sfx.
          * Negate the below condition.
          */
-        if (IsSEPlaying())
+        if (!IsSEPlaying())
         {
-//            Free(sEvoMovingBgPtr);
+            Free(sEvoMovingBgPtr);
             PlayCry1(gTasks[taskId].tPostEvoSpecies, 0);
             memcpy(&gPlttBufferUnfaded[0x20], sEvoStructPtr->savedPalette, 0x60);
             gTasks[taskId].tState++;

lioniac/pokefirered

src/evolution_scene.c
@@ -1195,9 +1195,9 @@ static void Task_TradeEvolutionScene(u8 taskId)
          * BUG: This check causes the evolved Pokemon's cry to play over the sfx.
          * Negate the below condition.
          */
-        if (IsSEPlaying())
+        if (!IsSEPlaying())
         {
-//            Free(sBgAnimPal);
+            Free(sBgAnimPal);
             PlayCry_Normal(gTasks[taskId].tPostEvoSpecies, 0);
             memcpy(&gPlttBufferUnfaded[0x20], sEvoStructPtr->savedPalette, sizeof(sEvoStructPtr->savedPalette));
             gTasks[taskId].tState++;